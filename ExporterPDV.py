b99f5ca8df317a2e0818971cdc42a02876abb9c7
Augusto Ferreira Werneck De Sa <vaugustof@indracompany.com>
2020-02-19 08:37:46 -0300

    Atualizacao ExporterPDV

--- ExporterPDV.py
+++ ExporterPDV.py
@@ -3,12 +3,24 @@ import argparse
 import os
 import Utils
 import pandas as pd
+import logging
+from datetime import datetime, date, timedelta
 
-############## Inicia processo #############################################
+############## Configs #####################################################
 with open('configs.json', 'r') as configFile:
     config = json.loads(configFile.read())
 entrada = config["pipelines"]
 arg_pipeline = "load-pdv"
+
+#Configs#####################################################################
+logger = logging.getLogger()
+logging.basicConfig(
+    filename="logs/{}-ATUALIZACAO_PDV.log".format(datetime.today().strftime("%Y-%m-%d")),
+    level=logging.INFO,
+    format='%(asctime)s: %(levelname)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
+dateNow = (datetime.today()  + timedelta(days=1)).strftime('%Y%m%d')
 
 # pipeline a ser executado###################################################
 pipelineInfo = [j for j in entrada if j["pipeline"] == arg_pipeline][0]
@@ -21,49 +33,48 @@ if pipelineInfo["sourceProtocol"] == "sftp":
 
     # verificar se existe arquivo no SFTP####################################
     if sftp.listdir():
-        print("### 1-INICIANDO PROCESSO DE MIGRACAO DA PLANILHA PDV DO SFTP ###")
-        # copia do arquivo sftp para local ##################################
-        copia_arquivo = Utils.copia_arquivo_sftp_para_local(sftp, sftp.listdir(), pipelineInfo["sourceFolderFTP"],
-                                                            pipelineInfo["folderConversao"],
-                                                            pipelineInfo["sourceFilePrefix"])
-
-        # tratamento do arquivo (Process_PDV_File.py) #######################
-        lista_arquivos = [files for files in os.listdir(pipelineInfo["folderConversao"]) if
-                          files.endswith(".csv") and files.startswith('PDV_RAW')]
-        for arquivo in lista_arquivos:
-            print("CONVERTENDO ARQUIVO:" + arquivo)
-            dfArquivo = pd.read_csv(pipelineInfo["folderConversao"] + "/" + arquivo, header=0, delimiter=";",
-                                    encoding='ISO-8859-2')[["UNIDADE", "PDV", "FORNECEDOR", "OPERACAO", "ILHA"]]
-            dfArquivo.to_csv(
-                pipelineInfo["destinationFolderConversao"] + "/" + arquivo.replace('PDV_RAW', 'PONTOS_DE_VENDA'),
-                index=None, sep=";", encoding='ISO-8859-2')
-
-        # move arquivos para o hdfs ########################################
-        print("### 2-INICIANDO PROCESSO DE MIGRACAO PARA HDFS ###")
-        returnHdfs = None
-        for files in os.listdir(pipelineInfo["destinationFolderConversao"]):
-            print("COPIANDO ARQUIVO PARA O HDFS: %s " % files)
-            returnHdfs = Utils.copia_arquivo_para_hdfs(pipelineInfo["destinationFolderConversao"], files,
-                                                       pipelineInfo["destinationFolderHDFS"])
-
-        # deleta os arquivos da staging (entrada e dados) ##################
-        path = [pipelineInfo["folderConversao"], pipelineInfo["destinationFolderConversao"]]
-        if copia_arquivo:
-            print("### 3-INICIANDO PROCESSO DE LIMPEZA DO STAGING ###")
-            for i in path:
-                for filename in os.listdir(i):
-                    print("REMOVENDO O ARQUIVO: %s DA STAGING: %s " % (filename, i))
-                    os.remove(i + filename)
-
-        # deleta os arquivos no ftp ########################################
-        if returnHdfs == 0:
-            print("### 4-INICIANDO PROCESSO DE LIMPEZA DO SFTP ###")
-            filesInRemoteArtifacts = sftp.listdir()
-            for file in filesInRemoteArtifacts:
-                print("REMOVENDO ARQUIVO DO SFTP: %s " % file)
-                sftp.remove(file)
-
+        for files in sftp.listdir():
+            if files == "PDV_RAW.csv":
+                print("### 1-INICIANDO PROCESSO DE MIGRACAO DA PLANILHA PDV DO SFTP ###")
+                # copia do arquivo sftp para local ##################################
+                copia_arquivo = Utils.copia_arquivo_sftp_para_local(sftp, pipelineInfo["sourceFolderFTP"], files, pipelineInfo["stagingFolderIn"])
+                print("COPIANDO ARQUIVO:" + files)
+                logger.info("Migrando arquivo do SFTP: {}".format(files))
+                # tratamento do arquivo (Process_PDV_File.py) #######################
+                fileConvertido = 'PONTOS_DE_VENDA_WEB_' + dateNow + '.csv'
+                print("CONVERTENDO ARQUIVO:" + fileConvertido)
+                dfArquivo = pd.read_csv(pipelineInfo["stagingFolderIn"] + "/" + files, header=0, delimiter=";",
+                                         encoding='ISO-8859-2')[["UNIDADE", "PDV", "FORNECEDOR", "OPERACAO", "ILHA"]]
+                dfArquivo.to_csv(pipelineInfo["stagingFolderOut"] + "/" + fileConvertido, index=None, sep=";", encoding='ISO-8859-2')
+                logger.info("Convertendo arquivo: {}".format(fileConvertido))
+                # move arquivos para o hdfs ########################################
+                print("### 2-INICIANDO PROCESSO DE MIGRACAO PARA HDFS ###")
+                returnHdfs = None
+                print("COPIANDO ARQUIVO PARA O HDFS: %s " % fileConvertido)
+                returnHdfs = Utils.copia_arquivo_para_hdfs(pipelineInfo["stagingFolderOut"], fileConvertido, pipelineInfo["destinationFolderHDFS"])
+                logger.info("Migrando arquivo para o HDFS: {}".format(fileConvertido))
+                if returnHdfs[0] == 1:
+                    print("FALHA AO MOVER ARQUIVO PARA O HDFS: {}".format(returnHdfs[1]))
+                    logger.error("Falha ao mover arquivo para o HDFS: {}".format(returnHdfs[1]))
+                # deleta os arquivos da staging (entrada e dados) ##################
+                path = [pipelineInfo["stagingFolderIn"], pipelineInfo["stagingFolderOut"]]
+                if copia_arquivo:
+                    print("### 3-INICIANDO PROCESSO DE LIMPEZA DO STAGING ###")
+                    for i in path:
+                        for filename in os.listdir(i):
+                            print("REMOVENDO O ARQUIVO: %s DA STAGING: %s " % (filename, i))
+                            os.remove(i + filename)
+                            logger.info("Removendo arquivo da staging: {}".format(filename))
+                # deleta os arquivos no sftp ########################################
+                if returnHdfs[0] == 0:
+                    print("### 4-INICIANDO PROCESSO DE LIMPEZA DO SFTP ###")
+                    print("REMOVENDO ARQUIVO DO SFTP: %s " % files)
+                    sftp.remove(files)
+                    logger.info("Removendo arquivo do SFTP: {}".format(files))
+            if files != 'PDV_RAW.csv':
+                print("### ARQUIVO {} NO SFTP FORA DO FORMATO 'PDV_RAW.CSV' ###".format(files))
+                logger.info("Arquivo {} no sftp fora do formato 'PDV_RAW.CSV' ###".format(files))
     else:
-        print("### NAO EXISTE ARQUIVO NO SFTP ###")
+        print("### SFTP VAZIO ###")
     # close da sessao
     sftp.close()
--- Utils.py
+++ Utils.py
@@ -1,8 +1,17 @@
+import ftplib
 import paramiko
+import subprocess
 import os
 
+# path de entrada para arquivos de cargas dos projetos
+HDFS_CARGAS_PATH = "/oi/digital/stage/ctl_cargas/"
+STAGING_CARGAS_PATH = "/data2/digital_stage/app/develop/scala/Load-digital/nsassis/dados/"
+FILE_ALL_WILDCARD = "*"
+
+
 ##################### SFTP  ############################
-def cria_conexao_sftp(host,porta,username,senha):
+
+def cria_conexao_sftp(host, porta, username, senha):
     """
     Funcao repsonsavel por criar a conexao sftp
     :param host:  nome  ou ip do servidor sftp
@@ -14,12 +23,13 @@ def cria_conexao_sftp(host,porta,username,senha):
     porta = int(porta)
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(host,porta,username,senha)
+    ssh.connect(host, porta, username, senha)
     sftp = ssh.open_sftp()
     sftp.sshclient = ssh
     return sftp
 
-def copia_arquivo_sftp_para_local(ftp_conn, list_files, remote_dir, local_dir, file_prefix):
+
+def copia_arquivo_sftp_para_local(ftp_conn, remote_dir, filename, local_dir):
     """
     Funcao responsavel por mover o(s) arquivo(s)  do  diretorio de ftp para o  diretorio local
     :param ftp_conn: conexao ftp
@@ -28,36 +38,211 @@ def copia_arquivo_sftp_para_local(ftp_conn, list_files, remote_dir, local_dir, f
     :param file_prefix: prefixo do arquivo
     """
     try:
-        for filename in list_files:
-            if filename.endswith(".csv") and filename.startswith(file_prefix):
-                print("COPIANDO ARQUIVO:" + filename)
-                ftp_conn.get(remote_dir + filename, local_dir + filename)
+        ftp_conn.get(remote_dir + filename, local_dir + filename)
         return True
     except Exception as e:
         print "Erro - %s" % e
         return False
 
-########## Local #############
-def verifica_existe_arquivos_local(local_dir,file_prefix):
+
+############### FTP ###################################
+
+# Funcao de criacao da conexao ftp
+def cria_conexao_ftp(host, username, senha):
     """
-    Funcao que verifica se existem arquivos no diretorio de staging
-    :param local_dir:
-    :param file_prefix:
+    Funcao responsavel por criar a conexao ftp
+    :param host: nome ou ip do servidor ftp
+    :param username: nome do usuario da conexao
+    :param senha: senha do usuario
+    :return: conexao ftp
+    """
+    ftpObj = ftplib.FTP(host)
+    ftpObj.login(username, senha)
+    return ftpObj
+
+
+def recupera_ftp_diretorios(ftp_conn, parentFolder):
+    """
+    Funcao responsavel pela captura da lista de subdiretorios do diretorio parent
+    :param ftp_conn: conexao ftp
+    :param parentFolder: diretorio no qual serao procurados os subdiretorios
+    :return: lista de diretorios
+    """
+    ftp_conn.cwd(parentFolder)
+    ret_list = []
+    ftp_conn.retrlines('LIST', ret_list.append)
+    folders = [x.replace(" ", "").split("<DIR>")[-1] for x in ret_list if "<DIR>" in x]
+    return folders
+
+
+def verifica_ftp_diretorio_existe(ftp_conn, parentFolder, folder_name):
+    """
+    Funcao responsavel pela verificacao de existencia de um subdiretorio X dado um dirtorio Y
+    :param ftp_conn: conexao ftp
+    :param parentFolder: caminho do diretorio a ser analisado
+    :param folder_name: nome do subdiretorio a ser procurado
+    :return: Booleano referente a existencia ou nao do subdiretorio
+    """
+    folders = recupera_ftp_diretorios(ftp_conn, parentFolder)
+    return folder_name in folders
+
+
+def cria_ftp_diretorio(ftp_conn, parentFolder, new_folder_name):
+    """
+    Funcao responsavel pela criacao de um subdiretorio X em um diretorio Y no servidor FTP
+    :param ftp_conn: conexao ftp
+    :param parentFolder: caminho do diretorio a ser analisado
+    :param new_folder_name: nome do subdiretorio a ser criado
+    :return: codigo de retorno da operacao make dir
+    """
+    codigo_retorno = ftp_conn.mkd(parentFolder + "/" + new_folder_name)
+    print "Diretorio criado no servidor FTP : %s/%s " % (parentFolder, new_folder_name)
+    return codigo_retorno
+
+
+def copia_arquivo_local_para_ftp(ftp_conn, local_dir, file_prefix, pipeline_date, ftp_dir):
+    """
+    Funcao responsavel por mover o(s) arquivo(s)  do  diretorio de staging para o  diretorio ftp
+    :param ftp_conn: conexao ftp
+    :param local_dir: diretorio de staging
+    :param file_prefix: prefixo do nome do arquivo
+    :param ftp_dir: diretorio ftp de destino
+    """
+    try:
+        for filename in os.listdir(local_dir):
+            if filename.endswith(".csv") and filename.startswith(file_prefix) and (pipeline_date in filename):
+                ftp_conn.cwd(ftp_dir + "/" + file_prefix)
+                ftp_conn.storbinary('STOR %s' % filename, open(local_dir + "/" + filename, 'rb'))
+                print "Arquivo " + filename + " enviado para " + ftp_dir + "/" + file_prefix + "/" + filename
+        return True
+    except Exception as e:
+        print "Erro - %s" % e
+        return False
+
+
+################  HDFS ###############################
+def verifica_safe_hdfs(hdfsPath):
+    """
+    Funcao responsavel por verificar se  o path em questao esta na estrutura padrao de cargas
+    :param hdfsPath:
+    :return: Boolean
+    """
+    if (hdfsPath.find(HDFS_CARGAS_PATH) == 0):
+        return True
+    else:
+        return False
+
+
+def verifica_hdfs_diretorio_existe(diretorio_hdfs):
+    """
+    Funcao repsonsavel por verificar se o diretorio do hdfs  informado existe no cluster
+    :param diretorio_hdfs:
+    :return: codigo de retorno da chamada hdfs - 0 sucesso ou 1 erro
+    """
+    print "Executando comando de verificacao de diretorio no hdfs:  hdfs dfs -test -e %s" % diretorio_hdfs
+    p1 = subprocess.Popen([" hdfs dfs -test -e %s" % diretorio_hdfs], stdout=subprocess.PIPE, shell=True)
+    output, errorInfo = p1.communicate()
+    return_code = p1.returncode
+    print "Fim da Verificacao de diretorio . [RETURN: %s, OUT: %s, ERROR: %s]" % (return_code, output, errorInfo)
+    return return_code
+
+
+def copiar_hdfs_files_para_local_staging(diretorio_origem_hdfs, file_prefix, pipeline_date, diretorio_destino):
+    """
+    Funcao responsavel pela copia dos arquivos do hdfs para  um diretorio local (client)
+    :param diretorio_origem_hdfs:  diretorio de  origem dos arquivos
+    :param hdfs_padroes_arquivos: pattern de nomenclatura  do arquivo
+    :param diretorio_destino: diretorio local (client) de  detino dos arquivos copiados
+    :return: codigo de retorno da execuao - 0 sucesso ou 1  erro
+    """
+
+    # remocao do identificador  unicode e  criacao de string unica com os  itens da lista
+    print "Executando comando de copia de arquivos:  hdfs dfs -copyToLocal %s/%s  %s" \
+          % (
+          diretorio_origem_hdfs, file_prefix + FILE_ALL_WILDCARD + pipeline_date + FILE_ALL_WILDCARD, diretorio_destino)
+    p1 = subprocess.Popen(["hdfs dfs -copyToLocal %s/%s  %s"
+                           % (
+                           diretorio_origem_hdfs, file_prefix + FILE_ALL_WILDCARD + pipeline_date + FILE_ALL_WILDCARD,
+                           diretorio_destino)], stdout=subprocess.PIPE, shell=True)
+    output, errorInfo = p1.communicate()
+    return_code = p1.returncode
+    print "Fim da copia de arquivos . [RETURN: %s, OUT: %s, ERROR: %s]" % (return_code, output, errorInfo)
+    return return_code
+
+
+def mover_hdfs_files(diretorio_origem_hdfs, file_prefix, pipeline_date, diretorio_destino_hdfs):
+    """
+    Funcao responsavel pela movimentacao de arwquivos entre diretorios do hdfs
+    :param diretorio_origem_hdfs:
+    :param hdfs_padroes_arquivos:
+    :param diretorio_historico_hdfs:
     :return:
     """
-    lista_arquivos = [files for files in os.listdir(local_dir) if files.endswith(".csv") and files.startswith(file_prefix)]
-    return True if len(lista_arquivos) > 0 else False
+    # strHDFSPadroesArquivos = ",".join([str(arq) for arq in hdfs_padroes_arquivos])  # remocao do identificador  unicode e  criacao de string unica com os  itens da lista
+    # strHDFSPadroesArquivos = str(file_prefix)  # remocao do identificador  unicode e  criacao de string unica com os  itens da lista
+    print "Executando comando de movimentacao de arquivos no hdfs:  hdfs dfs -mv %s/{%s}  %s" \
+          % (diretorio_origem_hdfs, file_prefix + FILE_ALL_WILDCARD + pipeline_date + FILE_ALL_WILDCARD,
+             diretorio_destino_hdfs)
+    p1 = subprocess.Popen(["hdfs dfs -mv %s/{%s}  %s"
+                           % (
+                           diretorio_origem_hdfs, file_prefix + FILE_ALL_WILDCARD + pipeline_date + FILE_ALL_WILDCARD,
+                           diretorio_destino_hdfs)], stdout=subprocess.PIPE, shell=True)
+    output, errorInfo = p1.communicate()
+    return_code = p1.returncode
+    print "Fim da movimentacao de arquivos . [RETURN: %s, OUT: %s, ERROR: %s]" % (return_code, output, errorInfo)
+    return return_code
 
-########## HDFS #############
-def copia_arquivo_para_hdfs(local_file, diretorio_hdfs):
+
+def copia_arquivo_para_hdfs(local_dir, local_file, diretorio_hdfs):
     """
-    Funcao repsonsavel por copiar o arquivo para o diretorio do hdfs no cluster
+    Funcao responsavel por copiar o arquivo para o diretorio do hdfs no cluster
     :param diretorio_hdfs: diretorio do hdfs
     :param local_file: arquivo a ser disponibilizado no hdfs
     :return: codigo de retorno da chamada hdfs - 0 sucesso ou 1 erro
     """
-    try:
-        p1 = subprocess.Popen([" hdfs dfs -put %s" % local_file + diretorio_hdfs],stdout=subprocess.PIPE,shell=True)
+    p1 = subprocess.Popen(["hdfs dfs -put %s/%s %s" % (local_dir, local_file, diretorio_hdfs)], stdout=subprocess.PIPE,
+                          stderr=subprocess.STDOUT, shell=True)
+    output, errorInfo = p1.communicate()
+    return_code = p1.returncode
+    return return_code, output
+
+
+########## Local #############
+
+def verifica_existe_arquivos_local(local_dir, file_prefix, pipeline_date):
+    """
+    Funcao que verifica se existem arquivos no diretorio de staging
+    :param local_dir:
+    :param file_prefix:
+    :param pipeline_date:
+    :return:
+    """
+    lista_arquivos = [files for files in os.listdir(local_dir) if
+                      files.endswith(".csv") and files.startswith(file_prefix) and (pipeline_date in files)]
+    return True if len(lista_arquivos) > 0 else False
+
+
+def limpa_staging(diretorio_staging, file_prefix, pipeline_date):
+    if (verifica_existe_arquivos_local(diretorio_staging, file_prefix, pipeline_date)):
+        print "Executando comando de limpeza de staging : rm  %s/%s*" % (diretorio_staging, file_prefix)
+        p1 = subprocess.Popen(["rm  %s/%s*" % (diretorio_staging, file_prefix)], shell=True)
         output, errorInfo = p1.communicate()
         return_code = p1.returncode
-    return return_code
+        print "Fim da limpeza do diretorio de staging . [RETURN: %s, OUT: %s, ERROR: %s]" % (
+        return_code, output, errorInfo)
+        return return_code
+    else:
+        print "Diretorio de staging vazio"
+        return 0
+
+
+def verifica_safe_local_staging(local_staging):
+    """
+    Funcao responsavel por verificar se  o path em questao esta na estrutura padrao de staging
+    :param local_staging:
+    :return: Boolean
+    """
+    if (local_staging.find(STAGING_CARGAS_PATH) == 0):
+        return True
+    else:
+        return False
